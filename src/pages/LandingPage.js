eb61588a4a7bda995edd1e32ad56e58d41838d7c
PhamAnhTu123 <tu.pham@nestsera.com>
2021-11-29 19:33:24 +0700

    base commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Typography } from '@mui/material';
+
+function LandingPage(){
+  return(
+    <div>
+      <Typography  sx={{ color: "red" }} variant='h1' align='center'>Hello World</Typography>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/style.js
@@ -0,0 +1,9 @@
+import { makeStyles } from ' ';
+
+const useStyles = makeStyles((theme) => ({
+  typo: {
+    textColor: 'red'
+  }
+}))
+
+export default useStyles;
